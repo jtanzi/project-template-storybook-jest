daf367e7254dea2762e219ddee826b7cf50e424f
Joel Tanzi <jtanziredux@gmail.com>
2020-07-23 21:25:17 -0500

    Scaffolding

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { render } from '@testing-library/angular';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -31,5 +32,12 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
     expect(compiled.querySelector('.content span').textContent).toContain('project-template-storybook-jest app is running!');
+  });
+
+  describe('AppComponent', () => {
+    it('should render the component', async () => {
+      const { getByText } = await render(AppComponent);
+      expect(getByText('Welcome'));
+    });
   });
 });
